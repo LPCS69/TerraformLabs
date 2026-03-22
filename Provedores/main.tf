3d93c2b5dc97d06bc5882b7a0f0e5956ad4667ad
Ubuntu <azureuser@kube02.gtekcky10aauvoeelnjyiijaye.cx.internal.cloudapp.net>
2026-03-22 22:18:53 +0000

    04.Provedores-07.Test

--- Provedores/main.tf
+++ Provedores/main.tf
@@ -1,7 +1,15 @@
 resource "local_file" "password" {
-    content = "mipassword"
-    filename = "password.tx"
-  
+  # 7.1 y 7.4
+  content  = "Password: ${random_string.iac_random.result}"
+
+  # Corregir nombre archivo
+  filename = "password.txt"
+
+  # 7.2
+  file_permission = "0400"
+
+  # 7.3
+  directory_permission = "0600"
 }
 resource "random_string" "iac_random" {
     length = 10
