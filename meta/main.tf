393e208d00c306494fd135f0edc327e496aa8059
Ubuntu <azureuser@kube02.gtekcky10aauvoeelnjyiijaye.cx.internal.cloudapp.net>
2026-03-25 06:13:46 +0000

    11.AWS

--- meta/main.tf
+++ meta/main.tf
@@ -1,5 +1,5 @@
 resource "local_sensitive_file" "tempfile_for" {
   for_each = toset(var.users)
-  filename          = each.value
-  content = "password: S3cr3tP@ssw0rd"
+filename = "${each.value}"
+content = "password: S3cr3tP@ssw0rd"
 }
