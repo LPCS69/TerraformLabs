c234010881711f55a0b9020be2cf9415bd6ae992
Ubuntu <azureuser@kube02.gtekcky10aauvoeelnjyiijaye.cx.internal.cloudapp.net>
2026-03-26 19:00:39 +0000

    PracticaFinal-commit4

--- PracticaFinal/main.tf
+++ PracticaFinal/main.tf
@@ -4,3 +4,11 @@ module "network" {
   subnet_cidr = lookup(var.subnet_cidr, terraform.workspace)
   tags        = var.tags
 }
+
+module "infra" {
+  source    = "./infra"
+  ami       = lookup(var.ami, terraform.workspace)
+  instancia = lookup(var.instancia, terraform.workspace)
+  subnet_id = module.network.subnet_id
+  tags      = var.tags
+}
