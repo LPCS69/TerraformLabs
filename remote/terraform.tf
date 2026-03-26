2d665074739746fdb9a7997b6343342805caa609
Ubuntu <azureuser@kube02.gtekcky10aauvoeelnjyiijaye.cx.internal.cloudapp.net>
2026-03-26 01:40:37 +0000

    13.Estado Remoto

---
+++ remote/terraform.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    key              = "terraform.tfstate"
+    region           = "us-east-1"
+    bucket           = "terralabs-remote-state-lpcs13" # Nombre del bucket creado en pasos
+  }
+}
